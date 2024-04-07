6dd4f4b62ebd95571fd44a2ccc87909ed16abcdc
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-04-07 11:58:14 -0300

    start

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.get("/", async (req, res) => {
-    const datas = await apiBook()
+    const datas = await apiImc()
     res.render("main", { datas })
 })
 
-async function apiBook() {
+async function apiImc() {
     const url = "https://api-imc.onrender.com/"
     const response = await fetch(url)
     return await response.json()
 }
 
 app.listen(8080, () => {
-    console.log("server online.")
+    console.log("server online.") 
 })
